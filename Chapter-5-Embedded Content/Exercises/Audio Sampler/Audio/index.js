4b553bff5f7b9dc72a2c6726878429667efbf770
NamithMNissam <namithmnissam@gmail.com>
2024-03-31 18:20:38 +0400

    chapter 5

---
+++ Chapter-5-Embedded Content/Exercises/Audio Sampler/Audio/index.js
@@ -0,0 +1,45 @@
+//AHAA
+function aha(){
+    var audio=document.getElementById("sound1")
+    sound1.play();
+}
+//DAN
+function dan(){
+    var audio=document.getElementById("sound2")
+    sound2.play();
+}
+//BACK OF THE NET
+function Back(){
+    var audio=document.getElementById("sound3")
+    sound3.play();
+}
+//BANG OUT OF ORDER
+function Bang(){
+    var audio=document.getElementById("sound4")
+    sound4.play();
+}
+//JURASSIC PARK
+function JURASSIC(){
+    var audio=document.getElementById("sound5")
+    sound5.play();
+}
+//I AM CONFUSED
+function CONFUSED(){
+    var audio=document.getElementById("sound6")
+    sound6.play();
+}
+//SCOTCH EGG
+function EGG(){
+    var audio=document.getElementById("sound7")
+    sound7.play();
+}
+//EMAIL OF THE EVENING
+function EMAIL(){
+    var audio=document.getElementById("sound8")
+    sound8.play();
+}
+//HELLO PATRIDGE
+function HELLO(){
+    var audio=document.getElementById("sound9")
+    sound9.play();
+}
